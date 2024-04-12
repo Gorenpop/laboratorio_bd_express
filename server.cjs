c98dd8e4bdf0550ccda67e4f5577f8797d3688dc
Gorenpop <144399352+Gorenpop@users.noreply.github.com>
2024-04-12 18:10:45 -0500

    changes

--- server.cjs
+++ server.cjs
@@ -1,11 +1,36 @@
 const express = require('express');
+const mysql = require('mysql2/promise');
+const cors = require('cors');
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 5173;
+
+
+app.use(cors());
+
+const pool = mysql.createPool({
+  host: 'ba4byiaeo8tt6rsdzuae-mysql.services.clever-cloud.com',
+  user: 'u43r7jmuzejzntyq',
+  database: 'ba4byiaeo8tt6rsdzuae',
+  password: 'Jx2v27HxcFX7jHqbWsZK',
+  port: 3306
+});
 
 app.get('/', (req, res) => {
-  res.send('¡Hola desde el servidor Express!');
+  res.send('Bienvenido al servidor Express');
+});
+
+app.get('/api/personas', async (req, res) => {
+  try {
+    const [rows] = await pool.execute('SELECT * FROM personas');
+    console.log(rows);
+    res.json(rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error retrieving data' });
+  }
 });
 
 app.listen(PORT, () => {
-  console.log(`El servidor se está ejecutando en http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
