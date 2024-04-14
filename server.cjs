baae6fdbd6c502537d4c9ccb2767b41fe038c522
Gorenpop <144399352+Gorenpop@users.noreply.github.com>
2024-04-13 21:30:07 -0500

    ultimos cambios

--- server.cjs
+++ server.cjs
@@ -6,10 +6,19 @@ const app = express();
 const PORT = 3001;
 
 // Configuración de CORS
-app.use(cors());
+// Configuración de CORS
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // Middleware para analizar el cuerpo de la solicitud como JSON
-app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
+
 
 // Configuración del pool de conexiones a la base de datos
 const pool = mysql.createPool({
