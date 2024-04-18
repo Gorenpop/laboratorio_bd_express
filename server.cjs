5279c06bcc91548320d9cf9a7ab7dc4208b73fcd
Gorenpop <144399352+Gorenpop@users.noreply.github.com>
2024-04-17 20:43:40 -0500

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -8,7 +8,7 @@ const PORT = 3001;
 // Configuración de CORS
 const corsOptions = {
   origin: function (origin, callback) {
-    const allowedOrigins = ['http://localhost:5173', 'https://laboratorio-bd-municipios.vercel.app'];
+    const allowedOrigins = ['http://localhost:5173', 'https://laboratorio-bd-municipios.vercel.app', 'https://laboratoriobdexpress-production.up.railway.app/'];
 
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
